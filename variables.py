571801a21396cf95310e8d0795e801975784bcea
Ibrahim Ndeje <98327282+Ibrah65@users.noreply.github.com>
2024-09-23 14:37:05 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,22 @@
+#python data types
+#cars = []
+#while True:
+   #values = input("Enter values:")
+   #if values == "stop":
+     #break
+   #cars.append(values)
+   #cars.sort()
+   #print(cars)
+#python data types
+cars = []
+while True:
+   values = input("Enter values: ")
+   if values == "stop":
+     break
+   cars.append(values)
+   cars.sort()
+   print(cars)
+
+# Print the total number of values
+print("Total number of values: ", len(cars))
+   
